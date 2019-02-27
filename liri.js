e283cf4df650c4ebca18b9c6cfc98278c8d85ea4
Ross carmack <rosscarmack@Rosss-MacBook-Pro.local>
2019-02-26 18:22:38 -0600

    updated to switch case

--- liri.js
+++ liri.js
@@ -24,76 +24,76 @@ var divider = "\n------------------------------------------------------------\n\
 //-----------OMDB----------//
 function OMDB(input) {
     axios.get("http://www.omdbapi.com/?t=" + input + "&apikey=trilogy")
-    .then(function (response) {
+        .then(function (response) {
 
             var showData = [response.data.Title,
-                            response.data.Year,
-                            response.data.imdbRating,
-                            response.data.Ratings[1].Source + ': ' + response.data.Ratings[1].Value,
-                            response.data.Country,
-                            response.data.Language,
-                            response.data.Plot,
-                            response.data.Actors].join('\n\n');
+            response.data.Year,
+            response.data.imdbRating,
+            response.data.Ratings[1].Source + ': ' + response.data.Ratings[1].Value,
+            response.data.Country,
+            response.data.Language,
+            response.data.Plot,
+            response.data.Actors].join('\n\n');
 
             console.log(showData)
-            
+
             append(showData)
         }
-    );
+        );
 };
 
 //-----------BandsInTown----------//
 //-----------BandsInTown----------//
 function BandsInTown(input) {
     axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-    .then(function(response){
-        for(var i = 0; i < response.data.length; i++) {
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
 
-            var date = response.data[i].datetime
+                var date = response.data[i].datetime
 
-            var showData = [response.data[i].venue.name,
-                            response.data[i].venue.city + ', ' + response.data[i].venue.country,
-                            moment(date).format('MM-DD-YYYY'),
-                            '-----------------'].join('\n\n')
-                            
-                            console.log(showData)
-            append(showData)
-        }
+                var showData = [response.data[i].venue.name,
+                response.data[i].venue.city + ', ' + response.data[i].venue.country,
+                moment(date).format('MM-DD-YYYY'),
+                    '-----------------'].join('\n\n')
 
-    })
+                console.log(showData)
+                append(showData)
+            }
+
+        })
 }
 
 //-----------Spotify----------//
 //-----------Spotify----------//
 function spotifySong(input) {
-    spotify.search({ type: 'track', query: input, limit: 5 }, function(err, data) {
+    spotify.search({ type: 'track', query: input, limit: 5 }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
 
-        for(var x = 0; x < data.tracks.items.length; x++) {
+        for (var x = 0; x < data.tracks.items.length; x++) {
 
             var showData = [data.tracks.items[x].album.artists[0].name,
-                            data.tracks.items[x].name,
-                            data.tracks.items[x].preview_url,
-                            data.tracks.items[x].album.name,
-                            '-----------------'].join('\n\n')
+            data.tracks.items[x].name,
+            data.tracks.items[x].preview_url,
+            data.tracks.items[x].album.name,
+                '-----------------'].join('\n\n')
 
             console.log(showData)
 
             append(showData)
         }
-     
-      });
+
+    });
 
 };
 
 //-----------FileSystem----------//
 //-----------FileSystem----------//
 function fileSystem() {
-    fs.readFile('./random.txt','utf8', (err, data) => {
+    fs.readFile('./random.txt', 'utf8', (err, data) => {
         if (err) throw err;
-        
+
         var newData = data.split(',')
 
 
@@ -106,60 +106,46 @@ function fileSystem() {
         } else {
             console.log('Make sure you have everything formatted correctly')
         }
-       
-      });
+
+    });
 }
 //-----------Append File----------//
 //-----------Append File----------//
 
 function append(showData) {
-    fs.appendFile("log.txt", showData + divider, function(err) {
+    fs.appendFile("log.txt", showData + divider, function (err) {
         if (err) throw err;
-       
-      });
+
+    });
 }
 
 
 //-----------If/Else Statements----------//
 //-----------If/Else Statements----------//
-if (action === 'concert-this') {
-    
-    if (input.length === 0) {
+switch (action) {
+    case 'concert-this':
+        var artist = input || 'Sylvan Esso';
+        BandsInTown(artist);
+        break;
 
-        BandsInTown('Sylvan Esso');
-    } else {
+    case 'spotify-this-song':
+        var song = input || 'Self Care'
+        spotifySong(song)
+        break;
 
-        BandsInTown(input);
-    }
+    case 'movie-this':
+        var movie = input || 'Mr Nobody'
+        OMDB(movie);
+        break;
 
-} else if (action === 'spotify-this-song') {
+    case 'do-what-it-says':
+        fileSystem();
+        break;
 
-        if (input.length === 0) {
-
-            spotifySong('Self Care')
-        } else {
-            
-            spotifySong(input)
-        }
-
-
-} else if (action === 'movie-this') {
-
-    if (input.length === 0) {
-
-        OMDB('Mr Nobody');
-    } else {
-
-        OMDB(input);
-    }
-
-} else if (action === 'do-what-it-says') {
-    
-    fileSystem();
-
-} else {
-    
-    console.log('Type in a command like "concert-this" followed by a band name.')
+    default:
+        console.log('Type in a command like "concert-this" followed by a band name.')
+        break;
+        
 }
 
 
